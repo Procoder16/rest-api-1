cef878976d24b0c9dec99f4a0aae05d711e03432
Procoder16 <smsoumik1608@gmail.com>
2022-04-14 20:13:56 +0530

    removed the bug

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,28 +7,34 @@ exports.register = (req, res, next) => {
   if(!errors.isEmpty()){
       return res.status(500).send('Validation failed!');
   }
-  const name = req.body.name;
-  const email = req.body.email;
-  const password = req.body.email;
-  bcrypt
-    .hash(password, 12)
-    .then((hashedPw) => {
-      const user = new User({
-        name: name,
-        password: hashedPw,
-        email: email,
-      });
-      user
-        .save()
-        .then((savedUser) => {
-          console.log("User created!");
-          res.status(200).send(savedUser);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+
+  User.findOne({email: req.body.email})
+    .then(userDoc => {
+        if(userDoc){
+            return res.status(500).send('Email already exists');
+        }
+        const name = req.body.name;
+        const email = req.body.email;
+        const password = req.body.email;
+        bcrypt
+            .hash(password, 12)
+            .then((hashedPw) => {
+                const user = new User({
+                name: name,
+                password: hashedPw,
+                email: email,
+            });
+            user
+            .save()
+            .then((savedUser) => {
+                console.log("User created!");
+                res.status(200).send(savedUser);
+            })
     })
-    .catch((err) => {
-      console.log(err);
-    });
+    }).catch(err => {
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+    });  
 }
--- routes/auth.js
+++ routes/auth.js
@@ -5,14 +5,6 @@ const { body } = require('express-validator');
 
 router.post("/register",[
     body('email').isEmail().isLength({min: 6}).withMessage('Please enter a valid email!')
-    .custom((value, { req }) => {
-        return User.findOne({email: value})
-               .then(userDoc => {
-                   if(userDoc){
-                       return Promise.reject('Email already exists');
-                   }
-               });
-    })
     .normalizeEmail(),
     body('password').trim().isLength({min: 8}),
     body('name').trim().not().isEmpty()
